264c1411df57cc80dcd0985ca5c5a8182f79048a
Tegar D. Lokananta <tegarlokananta.dev@gmail.com>
2024-04-30 23:55:03 +0700

    Update form validation in registerFormSchema

--- src/schema/form.js
+++ src/schema/form.js
@@ -26,10 +26,23 @@ export const updateKTP = z.object({
 });
 
 export const registerFormSchema = z.object({
-	username: z.string({ required_error: "Username is required" }).nonempty(),
-	firstName: z.string({ required_error: "First Name is required" }).nonempty(),
-	lastName: z.string({ required_error: "Last Name is required" }).nonempty(),
-	email: z.string({ required_error: "Email is required" }).email().nonempty(),
+	username: z
+		.string({ required_error: "Username is required" })
+		.min(4, { message: "Username must be at least 4 characters long, and must be unique" })
+		.nonempty(),
+	firstName: z
+		.string({ required_error: "First Name is required, without any special characters" })
+		.message("First Name is required")
+		.nonempty(),
+	lastName: z
+		.string({ required_error: "Last Name is required" })
+		.message("Last Name is required without any special characters")
+		.nonempty(),
+	email: z
+		.string({ required_error: "Email is required" })
+		.message("Please enter a valid email address, because we will send a verification email to this address.")
+		.email()
+		.nonempty(),
 	password: z
 		.string({ required_error: "Password is required" })
 		.min(8, { message: "Password must be at least 8 characters long" }),
